d3a88e47474017504cabf66084d94fcb5fc2741f
george <georgewk1809@gmail.com>
2024-11-29 20:12:00 +0700

    changes to fisioterapis page

--- src/app/fisioterapis/page.tsx
+++ src/app/fisioterapis/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from "next/navigation"
 import { getSession } from "../actions/getSession"
+import Navbar from "../components/navbar"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 const FisioterapisPage = async () => {
   const session = await getSession()
@@ -18,9 +21,15 @@ const FisioterapisPage = async () => {
 
   return (
     <div>
-      <h1>
-        Ini halaman Fisioterapis
-      </h1>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center min-h-screen gap-6 bg-gray-50">
+      <h1 className="text-2xl font-bold">Selamat Datang di halaman Fisioterapis</h1>
+        <div className="flex flex-col gap-4">
+          <Link href="/jadwalTerapi">
+            <Button variant="default" className="w-64">Jadwal Terapi</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
